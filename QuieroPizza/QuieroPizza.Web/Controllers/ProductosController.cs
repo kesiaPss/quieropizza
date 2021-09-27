42ddaab66892f4dba12dff3b2437f03660b6b3bf
Kesia Peralta <marilyn16.peralta@gmail.com>
2021-09-27 17:04:50 -0600

    Agregando arquitectura basica

--- QuieroPizza/QuieroPizza.Web/Controllers/ProductosController.cs
+++ QuieroPizza/QuieroPizza.Web/Controllers/ProductosController.cs
@@ -1,9 +1,9 @@
-﻿using QuieroPizza.Web.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TiendaOnline.BL;
 
 namespace QuieroPizza.Web.Controllers
 {
@@ -12,24 +12,9 @@ namespace QuieroPizza.Web.Controllers
         // GET: Productos
         public ActionResult Index()
         {
-            var producto1 = new ProductoModel();
-            producto1.Id = 1;
-            producto1.Descripcion = "Pizza 6 Quesos";
-
-            var producto2 = new ProductoModel();
-            producto2.Id = 2;
-            producto2.Descripcion = "Pizza 4 Estaciones";
-
-            var producto3 = new ProductoModel();
-            producto3.Id = 3;
-            producto3.Descripcion = "Pizza Jamon y Queso";
-
-            var listadeProductos = new List<ProductoModel>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
-
-
+            var productosBL = new ProductosBL();
+            var listadeProductos = productosBL.ObtenerProductos();
+           
             return View(listadeProductos);
         }
     }
---
+++ QuieroPizza/TiendaOnline.BL/ProductosBL.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiendaOnline.BL
+{
+    public class ProductosBL
+    {
+
+        public List<Producto> ObtenerProductos()
+        {
+            var producto1 = new Producto();
+            producto1.Id = 1;
+            producto1.Descripcion = "Camisa estilo polo";
+            producto1.Precio = 250;
+
+            var producto2 = new Producto();
+            producto2.Id = 2;
+            producto2.Descripcion = "Jeans licra";
+            producto2.Precio = 180;
+
+            var producto3 = new Producto();
+            producto3.Id = 3;
+            producto3.Descripcion = "Tenis";
+            producto3.Precio = 350;
+
+            var listadeProductos = new List<Producto>();
+            listadeProductos.Add(producto1);
+            listadeProductos.Add(producto2);
+            listadeProductos.Add(producto3);
+
+
+            return listadeProductos;
+        }
+    }
+}
+
+
